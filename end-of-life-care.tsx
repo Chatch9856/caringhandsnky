aa6c91cabeba2c86a3588451622595f76877cb37
Chatch9856 <techzaptalk@gmail.com>
2025-05-28 22:09:47 -0400

    Add files via upload

---
+++ end-of-life-care.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function EndOfLifeCare() {
+  return (
+    <div style={ padding: '2rem' }>
+      <h1>End Of Life Care</h1>
+      <p>We provide expert-level end of life care for patients in Northern Kentucky.</p>
+      <ul>
+        <li>Personalized care plans</li>
+        <li>Trusted professionals</li>
+        <li>Flexible scheduling</li>
+      </ul>
+      <button style={ marginTop: '1rem', padding: '0.75rem 1.5rem' }>Request Appointment</button>
+    </div>
+  );
+}
